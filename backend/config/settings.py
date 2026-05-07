10331cd0050ff1f45a2cc45e2c313f8f59e786d7
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2026-05-07 16:04:58 -0700

    ft: new seats conf 1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,6 +139,8 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "https://*.github.dev",
     "https://*.app.github.dev",
 ]
