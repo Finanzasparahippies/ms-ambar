02f58de1d6fa1884393340292253ef7049be7608
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2026-06-04 06:35:16 -0700

    plantillas cambiadas 1.8

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -164,6 +164,13 @@ REST_FRAMEWORK = {
     ),
 }
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+}
+
 # CORS and CSRF Settings
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
