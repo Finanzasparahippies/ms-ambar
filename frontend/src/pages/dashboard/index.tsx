d44b224b5fd4e84ed2f138b1a515473040eb53aa
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2026-07-30 14:07:31 -0700

    fix: cupones de descuento correcto 1.5

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import api from '../../lib/api';
 import { showAlert, showConfirm, showToast } from '../../lib/notifications';
+import { cn } from '../../lib/utils';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { motion, AnimatePresence } from 'framer-motion';
